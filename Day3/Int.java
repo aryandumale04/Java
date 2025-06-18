59394efa46b903e9c638e9349560ea34078c3f25
aryandumale04 <aryandumale04@gmail.com>
2025-06-18 09:50:36 +0530

    Add Int.java

---
+++ Day3/Int.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Int {
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the integer of your choice : ");
+        int b=sc.nextInt();
+        System.out.println("The entered integer is : "+b);
+        
+    }
+    
+}
