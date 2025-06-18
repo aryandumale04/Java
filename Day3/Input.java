c13cb83df412f153bf084a624219983f7fb4c6dc
aryandumale04 <aryandumale04@gmail.com>
2025-06-18 09:39:45 +0530

    Add Day-3

---
+++ Day3/Input.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Input {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a byte number of your choice : ");
+        byte b = sc.nextByte();
+        System.out.println("The entered byte is :  "+b);
+
+    }
+
+
+    
+}
