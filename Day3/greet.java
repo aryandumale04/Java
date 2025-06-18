4aadded25828265a68b82c25c724b32b93d57264
aryandumale04 <aryandumale04@gmail.com>
2025-06-18 11:00:42 +0530

    Add functions(methods)

---
+++ Day3/greet.java
@@ -0,0 +1,9 @@
+public class greet {
+    static void GREET(String name){
+        System.out.print("Hello "+name);
+    }
+    public static void main(String[] args){
+        GREET("Aryan");
+    }
+    
+}
---
+++ Day3/square.java
@@ -0,0 +1,13 @@
+public class square {
+
+    int sqrt(int n){
+        return n*n;
+    }
+    public static void main(String [] args){
+        square s1= new square();
+        int n =7;
+        int reuskt=s1.sqrt(n);
+        System.out.print(reuskt);cd
+    }
+    
+}
